db619f55af3adbf3cf72aa81cab65eed70cf02fb
Erez Naim <erezn@hackeruso.com>
2020-12-03 16:13:55 +0200

    removing logs

--- src/main/java/org/talkspace/automation/utils/FileUtil.java
+++ src/main/java/org/talkspace/automation/utils/FileUtil.java
@@ -1,27 +1,13 @@
 package org.talkspace.automation.utils;
 
-import org.apache.log4j.Logger;
-
 import java.io.File;
 
 public final class FileUtil {
-
-    private final static Logger Log = Logger.getLogger(FileUtil.class.getName());
 
     private FileUtil(){}
 
     public static String getFile(String path){
         return new File(System.getProperty("user.dir")).getParent() + path;
-    }
-
-    public static void createFolder(File folder , boolean recursive){
-        if(folder.exists() && folder.isDirectory()){
-            Log.info(folder.getName() + " directory already exist");
-        }else if((recursive ? folder.mkdirs() : folder.mkdir())){
-            Log.info(folder.getName() + " directory created successfully");
-        }else{
-            Log.error("failed to create '" + folder.getName() + "' directory");
-        }
     }
 }
 
